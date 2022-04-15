9c59972b556b598a97be06a174ed88ed122da6c7
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2022-04-15 15:42:25 +0300

    experiment

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <h2>Let's Start!!!</h2>
+      </>
+    );
+  }
+}
+
+export default App;
